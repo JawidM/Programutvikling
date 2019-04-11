3a891d998f817c309dce522f8b6b07e1faa215e1
JawidM <jawid93_love@yahoo.com>
2019-04-11 15:20:45 +0200

    NavnSetting er fullført
    Fikset navn på knappene. bruke de navnene som de knappene har. Ikke bytt navn på dem

--- src/main/java/org/openjfx/arrangementer/ArrangementerController.java
+++ src/main/java/org/openjfx/arrangementer/ArrangementerController.java
@@ -3,12 +3,34 @@ package org.openjfx.arrangementer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 
 
 public class ArrangementerController implements Initializable {
 
-    
+    @FXML
+    private ChoiceBox<?> chbSted;
+
+    @FXML
+    private TextField txtNavn;
+
+    @FXML
+    private TextField txtFornavn;
+
+    @FXML
+    private DatePicker dpDate;
+
+    @FXML
+    private Spinner<?> spiTid;
+
+    @FXML
+    private ComboBox<?> cobTypeArrangement;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/main/java/org/openjfx/billettsalg/BillettsalgController.java
+++ src/main/java/org/openjfx/billettsalg/BillettsalgController.java
@@ -3,11 +3,27 @@ package org.openjfx.billettsalg;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 
 
 public class BillettsalgController implements Initializable {
+    @FXML
+    private Spinner<?> spiAntallBilletter;
 
+    @FXML
+    private TextField txtTelefonnummer;
+
+    @FXML
+    private TextField txtEpost;
+
+    @FXML
+    private TextField txtEtternavn;
+
+    @FXML
+    private TextField txtFornavn;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
